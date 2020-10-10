b898dfbf65d9069e0bbf4457904e6d1759c3ea15
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-10 20:10:51 -0300

    refactor: remove dead code from UnknowBlockToHtml

--- src/data/usecases/blocks-to-html-converter/block-parsers/unknown.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/unknown.ts
@@ -1,13 +1,6 @@
-import { Block } from 'data/protocols/blocks';
 import { ToHtml } from 'domain/usecases/to-html';
 
 export class UnknownBlockToHtml implements ToHtml {
-  private _block: Block;
-
-  constructor(block: Block) {
-    this._block = block;
-  }
-
   async convert(): Promise<string> {
     return Promise.resolve('');
   }
