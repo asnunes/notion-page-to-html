fe5302df926fc2a85d4593e895a34baa481c9c10
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-10 12:20:54 -0300

    test: assert invalid path from notion.so throws InvalidPageUrlError

--- __tests__/infra/usecases/to-page-id/notion-url-to-page-id.test.ts
+++ __tests__/infra/usecases/to-page-id/notion-url-to-page-id.test.ts
@@ -3,12 +3,24 @@ import { InvalidPageUrlError } from '../../../../src/infra/errors/';
 
 describe('#toPageId', () => {
   describe('when invalid url is given', () => {
-    it('throws InvalidPageUrlError', () => {
-      const url = 'https://example.com/notion_page_id';
+    describe('when it is from another domain', () => {
+      it('throws InvalidPageUrlError', () => {
+        const url = 'https://example.com/notion_page_id';
 
-      const result = () => new NotionUrlToPageId(url).toPageId();
+        const result = () => new NotionUrlToPageId(url).toPageId();
 
-      expect(result).toThrow(new InvalidPageUrlError(url));
+        expect(result).toThrow(new InvalidPageUrlError(url));
+      });
+    });
+
+    describe('when it is from the same domain, but not a page path', () => {
+      it('throws InvalidPageUrlError', () => {
+        const url = 'https://www.notion.so/onboarding';
+
+        const result = () => new NotionUrlToPageId(url).toPageId();
+
+        expect(result).toThrow(new InvalidPageUrlError(url));
+      });
     });
   });
 });
