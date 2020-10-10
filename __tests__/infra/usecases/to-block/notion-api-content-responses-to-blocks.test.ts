d3c97ccbac2dd06a7377c382817560ae501a2693
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-10 00:44:08 -0300

    test: implement notion api content decoration to block for property content

--- __tests__/infra/usecases/to-block/notion-api-content-responses-to-blocks.test.ts
+++ __tests__/infra/usecases/to-block/notion-api-content-responses-to-blocks.test.ts
@@ -1,5 +1,5 @@
 import * as NotionApiMocks from '../../../mocks/notion-api-responses';
-import { Block, Decoration } from '../../../../src/data/protocols/blocks';
+import { Block, Decoration, DecorableText } from '../../../../src/data/protocols/blocks';
 import { NotionApiContentResponsesToBlocks } from '../../../../src/infra/usecases/to-blocks/notion-api-content-response-to-blocks';
 
 describe('#toBlocks', () => {
@@ -189,6 +189,39 @@ describe('#toBlocks', () => {
               decorations: [{ type: 'link', value: 'https://www.google.com' }],
             },
           ],
+        },
+      ]);
+    });
+  });
+
+  describe('when page with youtube link', () => {
+    it('converts to one block with decoration with value', () => {
+      const notionApiContentResponses = NotionApiMocks.TEXT_WITH_VIDEO_NOTION_API_CONTENT_RESPONSE;
+      const notionApiContentResponsesToBlocks = new NotionApiContentResponsesToBlocks(notionApiContentResponses);
+
+      const result = notionApiContentResponsesToBlocks.toBlocks();
+
+      expect(result).toEqual([
+        {
+          id: '80d0fc46-5511-4d1d-a4ec-8b2f43d75226',
+          type: 'text',
+          properties: {},
+          children: [] as Block[],
+          decorableTexts: [
+            {
+              text: 'Hello World',
+              decorations: [] as Decoration[],
+            },
+          ],
+        },
+        {
+          id: 'dcde43cb-7131-4687-8f22-c9789fa75f46',
+          type: 'video',
+          properties: {
+            source: 'https://www.youtube.com/watch?v=xBFqxBfLJWc',
+          },
+          children: [] as Block[],
+          decorableTexts: [] as DecorableText[],
         },
       ]);
     });
