d493d3babdb92587414e53b41e4ebf5d2db9ed8b
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-11 00:14:43 -0300

    test: replace color text to better hex version for brown

--- __tests__/data/usecases/blocks-to-html-converter/block-parsers/decorations/decoration-parsers/color.test.ts
+++ __tests__/data/usecases/blocks-to-html-converter/block-parsers/decorations/decoration-parsers/color.test.ts
@@ -65,4 +65,15 @@ describe('#convert', () => {
       expect(result).toBe('<span style="color: #9B9A97;">Text with color</span>');
     });
   });
+
+  describe('When brown color is given as background color', () => {
+    it('converts to equivalent hex code and apply style to html', async () => {
+      const text = 'Text with color';
+      const decoration: Decoration = { type: 'color', value: 'brown_background' };
+
+      const result = await new ColorDecorationToHtml(text, decoration).convert();
+
+      expect(result).toBe('<span style="background-color: #64473A;">Text with color</span>');
+    });
+  });
 });
