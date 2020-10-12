b1445edd3f8002d0b42934d25b1c89635d3226fe
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-12 01:24:24 -0300

    feat: add style to sub sub header block

--- src/data/usecases/blocks-to-html-converter/block-parsers/sub-sub-header.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/sub-sub-header.ts
@@ -1,6 +1,7 @@
 import { blockToInnerHtml } from '../../../helpers/block-to-inner-html';
 import { Block } from '../../../protocols/blocks';
 import { ToHtml } from '../../../../domain/usecases/to-html';
+import { FormatToStyle } from '../../../usecases/format-to-style';
 
 export class SubSubHeaderBlockParser implements ToHtml {
   private readonly _block: Block;
@@ -10,6 +11,7 @@ export class SubSubHeaderBlockParser implements ToHtml {
   }
 
   async convert(): Promise<string> {
-    return Promise.resolve(`<h3>${await blockToInnerHtml(this._block)}</h3>`);
+    const style = new FormatToStyle(this._block.format).toStyle();
+    return Promise.resolve(`<h3${style}>${await blockToInnerHtml(this._block)}</h3>`);
   }
 }
