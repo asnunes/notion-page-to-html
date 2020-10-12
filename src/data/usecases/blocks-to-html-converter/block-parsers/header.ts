93a0dbc0565bd0d0a4a3512a81c392cb0c45cd24
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-12 01:17:38 -0300

    feat: add style to header block

--- src/data/usecases/blocks-to-html-converter/block-parsers/header.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/header.ts
@@ -1,6 +1,7 @@
 import { blockToInnerHtml } from '../../../helpers/block-to-inner-html';
 import { Block } from '../../../protocols/blocks';
 import { ToHtml } from '../../../../domain/usecases/to-html';
+import { FormatToStyle } from '../../../usecases/format-to-style';
 
 export class HeaderBlockToHtml implements ToHtml {
   private readonly _block: Block;
@@ -10,6 +11,7 @@ export class HeaderBlockToHtml implements ToHtml {
   }
 
   async convert(): Promise<string> {
-    return Promise.resolve(`<h1>${await blockToInnerHtml(this._block)}</h1>`);
+    const style = new FormatToStyle(this._block.format).toStyle();
+    return Promise.resolve(`<h1${style}>${await blockToInnerHtml(this._block)}</h1>`);
   }
 }
