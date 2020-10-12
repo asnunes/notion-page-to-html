400f17a16d7ff74650dd81bc6c58a8d336be7504
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-12 01:47:40 -0300

    feat: add style to quote block

--- src/data/usecases/blocks-to-html-converter/block-parsers/quote.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/quote.ts
@@ -1,6 +1,7 @@
 import { blockToInnerHtml } from '../../../helpers/block-to-inner-html';
 import { Block } from '../../../protocols/blocks';
 import { ToHtml } from '../../../../domain/usecases/to-html';
+import { FormatToStyle } from '../../../usecases/format-to-style';
 
 export class QuoteBlockToHtml implements ToHtml {
   private readonly _block: Block;
@@ -10,6 +11,7 @@ export class QuoteBlockToHtml implements ToHtml {
   }
 
   async convert(): Promise<string> {
-    return Promise.resolve(`<blockquote>${await blockToInnerHtml(this._block)}</blockquote>`);
+    const style = new FormatToStyle(this._block.format).toStyle();
+    return Promise.resolve(`<blockquote${style}>${await blockToInnerHtml(this._block)}</blockquote>`);
   }
 }
