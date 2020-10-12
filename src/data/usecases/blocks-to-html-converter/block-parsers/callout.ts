aac5b7ab617a93d70f158bafcf98b3109297c44d
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-12 12:15:07 -0300

    feat: add style to callout

--- src/data/usecases/blocks-to-html-converter/block-parsers/callout.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/callout.ts
@@ -1,6 +1,7 @@
 import { blockToInnerHtml } from '../../../helpers/block-to-inner-html';
 import { Block } from '../../../protocols/blocks';
 import { ToHtml } from '../../../../domain/usecases/to-html';
+import { FormatToStyle } from '../../../usecases/format-to-style';
 
 export class CalloutBlockToHtml implements ToHtml {
   private readonly _block: Block;
@@ -10,8 +11,10 @@ export class CalloutBlockToHtml implements ToHtml {
   }
 
   async convert(): Promise<string> {
+    const style = new FormatToStyle(this._block.format).toStyle();
+
     return Promise.resolve(`
-    <div class="callout">
+    <div class="callout"${style}>
       <div class="callout-emoji">${this._block.properties.page_icon}</div>
       <p>${await blockToInnerHtml(this._block)}</p>
     </div>
