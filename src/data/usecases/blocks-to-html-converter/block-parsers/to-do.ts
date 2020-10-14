c75114b9c6bf21a019823d801a2c3194749d0236
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-14 11:31:20 -0300

    feat: implement children block for todo

--- src/data/usecases/blocks-to-html-converter/block-parsers/to-do.ts
+++ src/data/usecases/blocks-to-html-converter/block-parsers/to-do.ts
@@ -2,6 +2,7 @@ import { blockToInnerHtml } from '../../../helpers/block-to-inner-html';
 import { Block } from '../../../protocols/blocks';
 import { ToHtml } from '../../../../domain/usecases/to-html';
 import { FormatToStyle } from '../../../usecases/format-to-style';
+import { indentBlocksToHtml } from '../../../helpers/blocks-to-html';
 
 export class ToDoBlockToHtml implements ToHtml {
   private readonly _block: Block;
@@ -12,10 +13,12 @@ export class ToDoBlockToHtml implements ToHtml {
 
   async convert(): Promise<string> {
     const style = new FormatToStyle(this._block.format).toStyle();
+    const childrenHtml = await indentBlocksToHtml(this._block.children);
+
     return Promise.resolve(
       `<div${style}>
         <input type="checkbox"${this._isChecked() ? ' checked' : ''} name="${this._block.id}">
-        <label for="${this._block.id}">${await blockToInnerHtml(this._block)}</label>
+        <label for="${this._block.id}">${await blockToInnerHtml(this._block)}</label>${childrenHtml}
       </div>`,
     );
   }
