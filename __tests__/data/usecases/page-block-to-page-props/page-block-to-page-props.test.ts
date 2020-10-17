6cd3837d87dfb6905064a1017b92620310a87c41
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-17 16:38:42 -0300

    test: implement page block to page cover image

--- __tests__/data/usecases/page-block-to-page-props/page-block-to-page-props.test.ts
+++ __tests__/data/usecases/page-block-to-page-props/page-block-to-page-props.test.ts
@@ -1,24 +1,47 @@
+import nock from 'nock';
+import { resolve } from 'path';
 import { PageBlockToPageProps } from '../../../../src/data/usecases/page-block-to-page-props';
 import * as Blocks from '../../../mocks/blocks';
+import base64Img from '../../../mocks/img/base64';
 
 describe('#toPageProps', () => {
   describe('when page was title only', () => {
-    it('returns page prop with title only and correct value', () => {
+    it('returns page prop with title only and correct value', async () => {
       const pageBlockToPageProps = new PageBlockToPageProps(Blocks.PAGE_WITH_TITLE);
 
-      const result = pageBlockToPageProps.toPageProps();
+      const result = await pageBlockToPageProps.toPageProps();
 
       expect(result).toEqual({ title: 'Simple Page Title' });
     });
   });
 
   describe('when page was no title', () => {
-    it('returns page prop with title setted as an empty string', () => {
+    it('returns page prop with title setted as an empty string', async () => {
       const pageBlockToPageProps = new PageBlockToPageProps(Blocks.PAGE_WITHOUT_TITLE);
 
-      const result = pageBlockToPageProps.toPageProps();
+      const result = await pageBlockToPageProps.toPageProps();
 
       expect(result).toEqual({ title: '' });
+    });
+  });
+
+  describe('when page has title and cover image', () => {
+    describe('when image is from notion', () => {
+      beforeEach(() => {
+        nock('https://www.notion.so')
+          .get('/images/page-cover/solid_blue.png')
+          .replyWithFile(200, resolve('__tests__/mocks/img/baseImage.jpeg'), {
+            'content-type': 'image/jpeg',
+          });
+      });
+
+      it('return base64 image in coverImageSrc prop', async () => {
+        const pageBlockToPageProps = new PageBlockToPageProps(Blocks.PAGE_WITH_TITLE_AND_COVER_IMAGE);
+
+        const result = await pageBlockToPageProps.toPageProps();
+
+        expect(result).toEqual({ title: 'Simple Page Title', coverImageSrc: base64Img });
+      });
     });
   });
 });
