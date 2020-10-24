c589e19661879b0b2fb53d5e23af9c71af9755c5
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-10-24 13:53:47 -0300

    feat: implement page icon as notion image url to page props

--- src/data/usecases/page-block-to-page-props/page-block-to-icon.ts
+++ src/data/usecases/page-block-to-page-props/page-block-to-icon.ts
@@ -1,4 +1,5 @@
 import { Block } from '../../protocols/blocks';
+import { Base64Converter } from '../../../utils/base-64-converter';
 
 export class PageBlockToIcon {
   private readonly _pageBlock: Block;
@@ -10,7 +11,12 @@ export class PageBlockToIcon {
   async toIcon(): Promise<string | null> {
     const icon = this._pageBlock.format.page_icon;
     if (!icon) return Promise.resolve(null);
+    if (!icon.startsWith('http')) return icon;
 
-    return icon;
+    const url = `https://www.notion.so/image/${encodeURIComponent(icon)}?table=block&id=${this._pageBlock.id}`;
+    return Base64Converter.convert(url);
   }
 }
+
+// https://www.notion.so/image/https%3A%2F%2Fwww.example.com%2Fsome_image.png?table=block&id=4d64bbc0-634d-4758-befa-85c5a3a6c22f
+// https://www.notion.so/image/https%3A%2F%2Fwww.example.com%2Fsome_image.png?table=block&id=4d64bbc0-634d-4758-befa-85c5a3a6c22f
