402225ace99daca9aec6d697d0479d68b67ccdde
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-11-01 18:01:44 -0300

    refactor: replace node-fetch by https

--- src/main/factories/notion-api-page-fetcher.ts
+++ src/main/factories/notion-api-page-fetcher.ts
@@ -1,11 +1,7 @@
-import { isBrowser } from 'browser-or-node';
 import { NotionApiPageFetcher } from '../../infra/usecases/to-notion-api-content-responses/notion-api-page-fetcher';
+import { NodeHttpPostClient } from '../../infra/usecases/http-post/node-http-post-client';
 
 export const makeNotionApiPageFetcher = async (pageId: string): Promise<NotionApiPageFetcher> => {
-  const Client = isBrowser
-    ? (await import('../../infra/usecases/http-post/node-fetch-http-post-client')).NodeFetchHttpPostClient
-    : (await import('../../infra/usecases/http-post/node-http-post-client')).NodeHttpPostClient;
-
-  const httpPostClient = new Client();
+  const httpPostClient = new NodeHttpPostClient();
   return Promise.resolve(new NotionApiPageFetcher(pageId, httpPostClient));
 };
--- src/utils/base-64-converter.ts
+++ src/utils/base-64-converter.ts
@@ -1,4 +1,4 @@
-import { isBrowser } from 'browser-or-node';
+import { NodeHttpGetClient } from './usecases/http-get/node-http-get';
 
 export class Base64Converter {
   private readonly _imageSource: string;
@@ -12,11 +12,7 @@ export class Base64Converter {
   }
 
   async _convert(): Promise<string> {
-    const Client = isBrowser
-      ? (await import('./usecases/http-get/node-fetch-http-get')).NodeFetchHttpGetClient
-      : (await import('./usecases/http-get/node-http-get')).NodeHttpGetClient;
-
-    const response = await new Client().get(this._imageSource);
+    const response = await new NodeHttpGetClient().get(this._imageSource);
     return Promise.resolve(response.data.toString());
   }
 }
